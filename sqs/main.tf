93cab93b2c34ea0d305404712a966565da99f43b
Abraão Silva <abraaojs.dev@gmail.com>
2021-06-13 00:02:21 -0300

    change: add new tag

--- sqs/main.tf
+++ sqs/main.tf
@@ -1,8 +1,7 @@
 module "sqs" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "3.1.0"
-
-  name_sqs                    = "brk-geocoding"
+  name                        = "brk-geocoding"
   visibility_timeout_seconds  = 30
   delay_seconds               = 0
   max_message_size            = 262144
@@ -10,5 +9,4 @@ module "sqs" {
   receive_wait_time_seconds   = 0
   fifo_queue                  = false
   content_based_deduplication = false
-  environment                 = var.environment
 }
